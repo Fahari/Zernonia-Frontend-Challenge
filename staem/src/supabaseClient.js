e9a29d2d55bed5c442d70d7fc6050630a6917209
“Fahari” <karonjekevin67@gmail.com>
2022-10-09 14:31:35 +0300

    Modified supabaseClient.js for successful build

--- staem/src/supabaseClient.js
+++ staem/src/supabaseClient.js
@@ -1,8 +1,13 @@
+// @ts-nocheck
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
-const supabaseAuthorization = import.meta.env.VITE_PUBLIC_SUPABASE_AUTHORIZATION;
+// const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+// const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+// const supabaseAuthorization = import.meta.env.VITE_PUBLIC_SUPABASE_AUTHORIZATION;
+
+const supabaseUrl = 'https://gqkuommdmfzmwkzdewma.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdxa3VvbW1kbWZ6bXdremRld21hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDkyNjQyNTIsImV4cCI6MTk2NDg0MDI1Mn0.iF651HDhqynAQRlG8T6wFS3ZEx4dqxHiEiguc0m7-zI'
+const supabaseAuthorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZWNyZXQiOiI2MzNhODczMzk3Zjk4NmFhZDAzYTRlMDciLCJpYXQiOjE2NTE3NTIwNDN9.NOtlhNx5Y0KLI16KO0v46Uptzmn7llu35yVybuRNM4s'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
 	headers: { Authorization: supabaseAuthorization }
